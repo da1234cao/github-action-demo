996a70fb9ce997b6c82a82d9801e91ff90b79ac2
da1234cao <17355051286@163.com>
2023-06-18 22:15:19 +0800

    Update github-actions-demo.yml

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
 #include <QDebug>
+#include <boost/algorithm/algorithm.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+#include <iostream>
 
-int main(int argc, char *argv[]) { qDebug() << "hello world"; }
+int main(int argc, char *argv[]) {
+  qDebug() << "hello world";
+  std::string str = "hello world";
+  boost::to_upper(str);
+  std::cout << str << std::endl;
+}
